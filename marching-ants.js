d1d959da4390adae36b2d4ca9af1f35c6038ef2b
Eric Fortis <efortis@gmail.com>
2022-04-18 13:43:57 -0400

    tune

--- marching-ants.js
+++ marching-ants.js
@@ -6,7 +6,7 @@ window.addEventListener('load', function () {
 	function animateLine(elem) {
 		var SPEED = 0.2
 		var offset = 0
-		elem.style.strokeDasharray = '6 1'
+		elem.style.strokeDasharray = '6 2'
 		requestAnimationFrame(function anim() {
 			offset -= SPEED // Without this extra variable, it doesn't work in FF and Safari
 			elem.style.strokeDashoffset = offset
