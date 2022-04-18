c99a243b1d71aaafbf00437cb5da029d30d1f736
Eric Fortis <efortis@gmail.com>
2022-04-18 15:23:13 -0400

    refactor

--- marching-ants.js
+++ marching-ants.js
@@ -1,16 +1,13 @@
 window.addEventListener('load', function () {
-	const animatedLines = document.getElementsByClassName('marchingAnts')
-	for (var i = 0; i < animatedLines.length; i++)
-		animateLine(animatedLines[i])
-
-	function animateLine(elem) {
-		var SPEED = 0.2
-		var offset = 0
-		elem.style.strokeDasharray = '6 2'
-		requestAnimationFrame(function anim() {
-			offset -= SPEED // Without this extra variable, it doesn't work in FF and Safari
-			elem.style.strokeDashoffset = offset
-			requestAnimationFrame(anim)
+	document.querySelectorAll('.marchingAnts').forEach(
+		function animateLine(elem) {
+			const SPEED = 0.2
+			let offset = 0
+			elem.style.strokeDasharray = '6 2'
+			requestAnimationFrame(function anim() {
+				offset -= SPEED // Without this extra variable, it doesn't work in FF and Safari
+				elem.style.strokeDashoffset = offset
+				requestAnimationFrame(anim)
+			})
 		})
-	}
 })
