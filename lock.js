8f05cd4f03a77fcedb43a5e94588fba3b721c787
Eric Fortis <efortis@gmail.com>
2022-04-18 22:14:24 -0400

    lock

---
+++ lock.js
@@ -0,0 +1,86 @@
+window.addEventListener('load', function () {
+
+	AnimateLock()
+	function AnimateLock() {
+		const Lock = document.getElementById('Lock')
+		Lock.removeEventListener('click', AnimateLock)
+
+		const LockShape = Lock.getElementById('LockShape')
+
+		let delay = 0
+		let lastRotation = 0
+		let duration = 400
+		let rotation = 11
+
+		rotateLock(lastRotation, rotation, duration, delay)
+
+		lastRotation = rotation
+		rotation = -12
+		delay += duration
+		duration = 300
+		rotateLock(lastRotation, rotation, duration, delay)
+
+		lastRotation = rotation
+		rotation = 8
+		delay += duration
+		duration = 300
+		rotateLock(lastRotation, rotation, duration, delay)
+
+		lastRotation = rotation
+		rotation = -6
+		delay += duration
+		duration = 300
+		rotateLock(lastRotation, rotation, duration, delay)
+
+		lastRotation = rotation
+		rotation = 4
+		delay += duration
+		duration = 300
+		rotateLock(lastRotation, rotation, duration, delay)
+
+		lastRotation = rotation
+		rotation = -2
+		delay += duration
+		duration = 200
+		rotateLock(lastRotation, rotation, duration, delay)
+
+		lastRotation = rotation
+		rotation = 0
+		delay += duration
+		duration = 200
+		rotateLock(lastRotation, rotation, duration, delay)
+
+		setTimeout(function throttle() {
+			Lock.addEventListener('click', AnimateLock)
+		}, duration + delay)
+
+		function rotateLock(startRotation, endRotation, durationMs, delayMs) {
+			setTimeout(() => animateRotation({
+				element: LockShape,
+				centerX: 75,
+				centerY: 27,
+				startRotation,
+				endRotation,
+				durationMs
+			}), delayMs)
+		}
+	}
+
+	function animateRotation(o) {
+		animate()
+		function animate() {
+			const start = Date.now()
+			requestAnimationFrame(function anim() {
+				const time = Math.min(1, (Date.now() - start) / o.durationMs)
+				const angle = easeOutQuad(time) * (o.endRotation - o.startRotation) + o.startRotation
+				o.element.setAttribute('transform', `rotate( ${angle}, ${o.centerX}, ${o.centerY})`)
+				if (time < 1)
+					requestAnimationFrame(anim)
+			})
+		}
+	}
+
+	function easeOutQuad(x) { // https://easings.net/#easeOutQuad
+		return x * (2 - x)
+	}
+})
