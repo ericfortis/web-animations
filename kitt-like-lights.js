61f21d0db8e5321dde851258af7db67ad6eb3661
Eric Fortis <efortis@gmail.com>
2022-05-17 14:03:55 -0400

    refactor Kitt lights

--- kitt-like-lights.js
+++ kitt-like-lights.js
@@ -4,83 +4,80 @@
 // Each light is represented by a bit on an integer. The algorithm shifts bits left
 // or right to produce the sequence. The `on` CSS class is what makes the light shine.
 
-window.addEventListener('load', function () {
-	const cKitt = 'Kitt'
-	const cLight = 'light'
-	const cOn = 'on'
+if (typeof window !== 'undefined')
+	window.addEventListener('load', async function () {
+		const cKitt = 'Kitt'
+		const cLight = 'light'
+		const cOn = 'on'
+		const N_LIGHTS = 6
+		const SPEED_MS = 166
 
-	const N_LIGHTS = 6
-	const SPEED_MS = 166
+		const Lights = []
+		for (let i = 0; i < N_LIGHTS; i++) {
+			const light = document.createElement('div')
+			light.className = cLight
+			Lights.push(light)
+		}
+		document.getElementsByClassName(cKitt)[0].append(...Lights)
 
-	const binaryMaxValue = 2 ** N_LIGHTS - 1 // 0b111111
-	const leftmostBit = 1 << (N_LIGHTS - 1)  // 0b100000
-
-	const Kitt = document.getElementsByClassName(cKitt)[0]
-	for (let i = 0; i < N_LIGHTS; i++) {
-		const light = document.createElement('div')
-		light.className = cLight
-		Kitt.append(light)
-	}
-
-	const Lights = Kitt.getElementsByClassName(cLight)
-
-	const seq = makeSeq();
-	(async function () {
+		const seq = makeKittSeq(N_LIGHTS)
 		for (; ;)
 			for (const s of seq) {
 				for (let i = 0; i < Lights.length; i++)
 					Lights[i].classList.toggle(cOn, s[i])
 				await new Promise(resolve => setTimeout(resolve, SPEED_MS)) // delay
 			}
-	}())
+	})
 
 
-	function makeSeq() {
-		const states = []
-		let lights = 1
+function makeKittSeq(nLights) {
+	const binaryMaxValue = 2 ** nLights - 1 // 0b111111
+	const leftmostBit = 1 << (nLights - 1)  // 0b100000
 
-		while (lights < leftmostBit) {
-			lights = lights << 1
-			states.push(lights)
-		}
+	const states = []
+	let lights = 1
 
-		while (lights > 1) {
-			lights = lights >> 1
-			states.push(lights)
-		}
-
-		let rev = 1
-		while (lights < binaryMaxValue) {
-			rev = rev << 1
-			lights += rev
-			states.push(lights)
-		}
-
-		rev = 1
-		while (lights > leftmostBit) {
-			lights -= rev
-			rev = rev << 1
-			states.push(lights)
-		}
-
-		rev = leftmostBit
-		while (lights < binaryMaxValue) {
-			rev = rev >> 1
-			lights += rev
-			states.push(lights)
-		}
-
-		rev = leftmostBit
-		while (lights > 1) {
-			lights -= rev
-			rev = rev >> 1
-			states.push(lights)
-		}
-
-		return states.map(lights => lights
-			.toString(2)
-			.padStart(N_LIGHTS, '0')
-			.split('')
-			.map(Number))
+	while (lights < leftmostBit) {
+		lights = lights << 1
+		states.push(lights)
 	}
-})
+
+	while (lights > 1) {
+		lights = lights >> 1
+		states.push(lights)
+	}
+
+	let rev = 1
+	while (lights < binaryMaxValue) {
+		rev = rev << 1
+		lights += rev
+		states.push(lights)
+	}
+
+	rev = 1
+	while (lights > leftmostBit) {
+		lights -= rev
+		rev = rev << 1
+		states.push(lights)
+	}
+
+	rev = leftmostBit
+	while (lights < binaryMaxValue) {
+		rev = rev >> 1
+		lights += rev
+		states.push(lights)
+	}
+
+	rev = leftmostBit
+	while (lights > 1) {
+		lights -= rev
+		rev = rev >> 1
+		states.push(lights)
+	}
+
+	return states.map(lights => lights // Numbers to BitArrays
+		.toString(2)
+		.padStart(nLights, '0')
+		.split('')
+		.map(Number))
+}
